f3f23ab6f5f5730622ef16d1b927b95e07dc7d4e
Your Name <you@example.com>
2020-11-23 16:34:02 +0300

    se

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,29 +1,30 @@
 import React from 'react'
-
+import './login.css'
+import doctor from '../../images/logo.jpg'
 function login() {
     return (
-        <div>
+        <div className="page" >
             <div>
-                <h2>Login here</h2>
+                <h2>Welcome!</h2>
             </div>
-            <div>
+            <div className="log">
+              <img className="kl" src={doctor}/>
+            </div>
+            <div className="form-carrier">
             <form>
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
   </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  
 </form>
             </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+
         </div>
     )
 }
